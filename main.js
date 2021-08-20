85f77cc0691637e65a63d4622949d43c671f31c5
Nash_Traineee <55914156+TraineeNash@users.noreply.github.com>
2021-08-20 12:00:15 +0800

    add img link

---
+++ main.js
@@ -0,0 +1,27 @@
+// 1-
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg
+
+// 2-
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg
+
+// 3-
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg
+
+// 4-
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg
+
+// 5-
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg
+
+// 6- 
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg
+
+// 7- 
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg
+
+// 8- 
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg
+
+// 9- 
+// https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg
+
